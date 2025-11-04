85f4168398de2ea3036f911bbd991660dfae610d
kasperheinze <s245220@dtu.dk>
2025-11-04 12:53:21 +0100

    junit test for Enemy :fire:

---
+++ core/src/test/java/io/github/cheatinneed/tower_defense/model/Enemies/BasicEnemyTest.java
@@ -0,0 +1,33 @@
+package io.github.cheatinneed.tower_defense.model.Enemies;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicEnemyTest {
+
+    @Test
+    void constructorInitializesDefaults() {
+        Enemy enemy = EnemyFactory.createEnemy("basic", 2.5f, 7.75f, 999);
+        assertTrue(enemy instanceof BasicEnemy);
+        assertEquals(2.5f, enemy.getX(), 0.0001f);
+        assertEquals(7.75f, enemy.getY(), 0.0001f);
+        assertEquals(1, enemy.getCurrentHealth(), "BasicEnemy should start with currentHealth = 1");
+        assertEquals(1, enemy.getMaxHealth(), "BasicEnemy should have maxHealth = 1");
+    }
+
+    @Test
+    void setPositionUpdatesCoordinates() {
+        Enemy enemy = EnemyFactory.createEnemy("basic", 0f, 0f, 999);
+        enemy.setPosition(10.0f, 20.0f);
+        assertEquals(10.0f, enemy.getX(), 0.0001f);
+        assertEquals(20.0f, enemy.getY(), 0.0001f);
+    }
+
+    @Test
+    void setCurrentHealthChangesHealth() {
+        Enemy enemy = EnemyFactory.createEnemy("basic", 0f, 0f, 999);
+        enemy.setCurrentHealth(0);
+        assertEquals(0, enemy.getCurrentHealth());
+    }
+}
---
+++ core/src/test/java/io/github/cheatinneed/tower_defense/model/Enemies/CustomEnemyTest.java
@@ -0,0 +1,34 @@
+package io.github.cheatinneed.tower_defense.model.Enemies;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomEnemyTest {
+
+    @Test
+    void constructorSetsMaxAndCurrentHealth() {
+        Enemy enemy = EnemyFactory.createEnemy("custom", 1.25f, -3.5f, 42);
+        assertTrue(enemy instanceof CustomEnemy);
+        assertEquals(1.25f, enemy.getX(), 0.0001f);
+        assertEquals(-3.5f, enemy.getY(), 0.0001f);
+        assertEquals(42, enemy.getCurrentHealth(), "CustomEnemy should start with currentHealth = maxHealth");
+        // This is the expected behavior; if this assertion fails, there may be a bug in getMaxHealth implementation
+        assertEquals(42, enemy.getMaxHealth(), "CustomEnemy should report its maxHealth value");
+    }
+
+    @Test
+    void canChangeHealth() {
+        Enemy enemy = EnemyFactory.createEnemy("custom", 0f, 0f, 5);
+        enemy.setCurrentHealth(3);
+        assertEquals(3, enemy.getCurrentHealth());
+    }
+
+    @Test
+    void setPositionWorks() {
+        Enemy enemy = EnemyFactory.createEnemy("custom", 0f, 0f, 10);
+        enemy.setPosition(4.5f, 9.0f);
+        assertEquals(4.5f, enemy.getX(), 0.0001f);
+        assertEquals(9.0f, enemy.getY(), 0.0001f);
+    }
+}
