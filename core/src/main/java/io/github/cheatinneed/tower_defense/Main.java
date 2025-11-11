cef3c62c3e93572cb98d89bd59e7f805260e2156
PeterWulff1 <peter@wulff1.dk>
2025-11-11 14:15:54 +0100

    made mainmenu and buttons, cleaned main a bit

--- core/src/main/java/io/github/cheatinneed/tower_defense/Main.java
+++ core/src/main/java/io/github/cheatinneed/tower_defense/Main.java
@@ -6,29 +6,62 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import io.github.cheatinneed.tower_defense.model.enemies.EnemyManager;
 import io.github.cheatinneed.tower_defense.model.path.Path;
 import io.github.cheatinneed.tower_defense.model.path.PathPoint;
 import io.github.cheatinneed.tower_defense.model.waves.WaveManager;
 import io.github.cheatinneed.tower_defense.view.EnemyRenderer;
+import io.github.cheatinneed.tower_defense.view.GameView;
+import io.github.cheatinneed.tower_defense.view.MainMenuView;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main extends ApplicationAdapter {
-
     private SpriteBatch batch;
-    private Texture mapTexture;
+    private FitViewport viewport;
+    private static final int viewport_width = 1536;
+    private static final int viewport_height = 1024;
 
+    //menu
+    private Texture mainMenuTexture;
+    private MainMenuView mainMenuView;
+
+    //game
+    private Texture mapTexture;
     private WaveManager waveManager;
     private EnemyManager enemyManager;
     private Path path;
 
+    private GameView gameView;
+
+    private boolean showMenu = true; // flag for om vi er i menu eller i spil
+
+
     @Override
     public void create() {
         batch = new SpriteBatch();
+        // sikre at vinduet er den rigtige størrelse
+        viewport = new FitViewport(viewport_width, viewport_height);
+        viewport.apply();
+
+        //assets
         mapTexture = new Texture("TDmap2.png");
+        mainMenuTexture = new Texture("MainMenu.png");
+
+        //menu view
+        mainMenuView = new MainMenuView(batch, mainMenuTexture);
+        mainMenuView.initUi(viewport);                  // (NY) byg Stage + knapper
+        Gdx.input.setInputProcessor(mainMenuView.getStage());
+
+        //game view
+        gameView = new GameView(batch,viewport);
+        gameView.setMapTexture(mapTexture);
 
         // Build path
         int mapHeight = 1024; // image height
@@ -60,25 +93,33 @@ public class Main extends ApplicationAdapter {
 
         waveManager = new WaveManager(path);
         enemyManager = EnemyManager.getInstance();
+
+        //knap logik
+        mainMenuView.getPlayButton().addListener(evt -> {
+            if (mainMenuView.getPlayButton().isPressed()) {
+                showMenu = false;
+                Gdx.input.setInputProcessor(null); // skift til spillets input når du er klar
+            }
+            return false;
+        });
+
+        mainMenuView.getExitButton().addListener(evt -> {
+            if (mainMenuView.getExitButton().isPressed()) {
+                Gdx.app.exit();
+            }
+            return false;
+        });
     }
 
     @Override
     public void render() {
-        float dt = Gdx.graphics.getDeltaTime();
-
-        waveManager.update(dt);
-        enemyManager.update();
-
-        draw();
-    }
-
-    private void draw() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        batch.draw(mapTexture, 0, 0);
-        EnemyRenderer.draw(batch);
-        batch.end();
+        if(showMenu){mainMenuView.render();}
+        else{
+            float dt = Gdx.graphics.getDeltaTime();
+            waveManager.update(dt);
+            enemyManager.update();
+            gameView.render(dt);
+        }
     }
 
 
@@ -86,5 +127,6 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         mapTexture.dispose();
+        if(mainMenuView!=null){mainMenuView.dispose();}
     }
 }
