dbd75c03064ed3f990dee354ade5fb672a2763f0
Hatiti <ludvig1354@gmail.com>
2025-11-19 16:49:58 +0100

    added FlameTower

---
+++ core/src/main/java/io/github/cheatinneed/tower_defense/model/towers/FlameTower.java
@@ -0,0 +1,23 @@
+package io.github.cheatinneed.tower_defense.model.towers;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlameTower extends AbstractTower{
+    public FlameTower(float x, float y) {
+        super(x, y);
+        this.range = 180f;
+        this.damage = 15f;
+        this.fireRate = 1.2f;
+        this.projectileSpeed = 500f;
+    }
+    @Override
+    protected void onUpdate(float dt) {
+        // rotate/animate if you’d like
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        // If TowerRenderer handles visuals, you can keep this empty.
+        // Otherwise, draw the tower sprite at (x,y) here.
+    }
+}
