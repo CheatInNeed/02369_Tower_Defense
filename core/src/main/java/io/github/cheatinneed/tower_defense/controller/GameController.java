81b677a63220e88597d5630da361149136b09736
CheatInNeed <peterroland02@gmail.com>
2025-11-20 20:59:37 +0100

    added economy so that when a wave is finished, player gets money.

--- core/src/main/java/io/github/cheatinneed/tower_defense/controller/GameController.java
+++ core/src/main/java/io/github/cheatinneed/tower_defense/controller/GameController.java
@@ -14,6 +14,7 @@ public class GameController {
     private final ProjectileManager projectileManager;
 
     private boolean paused = false;
+    private boolean wasWaveRunning = false;
     private final Player player = new Player();
 
     public GameController(WaveManager waveManager) {
@@ -31,9 +32,16 @@ public class GameController {
         towerManager.update(dt);
         projectileManager.update(dt);
 
+        boolean isWaveRunning = waveManager.isWaveRunning();
+        if (wasWaveRunning && !isWaveRunning && !player.isDead()) {
+            System.out.println("Player got money for completing wave!");
+            player.addMoney(50);
+        }
+        wasWaveRunning = isWaveRunning;
+
         //System.out.println(player.getLives());
         if (player.isDead()) {
-            System.out.println("PLAYER IS DEAD");
+            //System.out.println("PLAYER IS DEAD");
             // TODO - Handle game over. Game over screen skal vises, skal spillet pauses her.
             //pause();
         }
